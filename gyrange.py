0decc625a42a5005ff3f61766debd742e1810331
Noob99n <loomjoom07@gmail.com>
2024-12-20 17:43:31 +0530

    Update gyrange.py

--- gyrange.py
+++ gyrange.py
@@ -6,7 +6,7 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7380126811:AAGXDmWjnMpwUaxVUeqwX9K1nofPNpAq4T4')
+bot = telebot.TeleBot('7817888758:AAEPZFAEtc-zlp4FOb4VDwwzt5k-faKzTHQ')
 
 # Admin user IDs
 admin_id = {"1721730214"}
